043c8835c24de2c19c410b7e62063cae5a3f0982
soumany <soumany06@gmail.com>
2022-11-03 22:57:34 +0700

    3 graph

--- Multi_Bar_Graph.py
+++ Multi_Bar_Graph.py
@@ -9,13 +9,17 @@ df = pd.read_csv('Test graph.csv')
 x = df['Country']
 x_indexes = np.arange(len(x))
 width = 0.25
-
+#popularity
 y = df['Percentage of popularity']
 plt.bar(x_indexes - width  ,y, width = width ,  color="#e5ae38", label ="Popularity")
 
+#Safety
 safe_y = df['Percentage of safty']
 plt.bar(x_indexes ,safe_y, width = width,  color="#008fd5", label ="Safety")
 
+#Total
+total_y = df['Percentage of total']
+plt.bar(x_indexes+width , total_y, width = width, color="#0000EE", label= "Total")
 #Bar Chart
 plt.legend()
 
@@ -23,7 +27,7 @@ plt.xticks(ticks = x_indexes, labels = x)
 plt.title('Top 10 countries of Quality of Life', fontsize=13)
 plt.xlabel('Country', fontsize=13)
 plt.ylabel('Percentage', fontsize=13)
-#plt.bar(x, y)
+
 
 plt.tight_layout()
 plt.show()
